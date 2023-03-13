df4358702dbcfad32cd233ccffe373a8c4046883
Filip <filip.rak.20001@gmail.com>
2023-03-13 16:28:44 +0100

    Initial commit

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,42 @@
+import java.io.CharConversionException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringCalculator {
+
+
+    public int retNumber(String number) throws Exception{
+        if(number.isEmpty()){
+            return 0;
+        }
+        if(number.length() == 1){
+            return Character.getNumericValue(number.charAt(0));
+        }
+        if(number.charAt(0) == '-'){
+            throw new Exception("Negative number");
+        }
+        List<Integer> numbers = new ArrayList<Integer>();
+        boolean isNewNumber = true;
+        for(int i = 0; i < number.length(); i++){
+            if(Character.isDigit(number.charAt(i))){
+                int num = Character.getNumericValue(number.charAt(i));
+                if(isNewNumber){
+                    numbers.add(num);
+                    isNewNumber = false;
+                }else {
+                    int newNum = numbers.get(numbers.size()-1)*10 + num;
+                    numbers.set(numbers.size()-1, newNum);
+                }
+            }
+            else {
+                isNewNumber = true;
+            }
+        }
+        int sum = 0;
+        for (Integer integer: numbers
+             ) {
+                sum += integer;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/StringCalculatorTests.java
@@ -0,0 +1,46 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class StringCalculatorTests {
+
+    StringCalculator calculator = new StringCalculator();
+
+
+    @Test
+    void zeroElement() throws Exception{
+            assertEquals(0, calculator.retNumber(""));
+    }
+    @Test
+    void oneNUmber()throws Exception{
+            assertEquals(1, calculator.retNumber("1"));
+    }
+    @Test
+    void twoNumbers() throws Exception{
+            assertEquals(7, calculator.retNumber("2,5"));
+            assertEquals(15, calculator.retNumber("5\n10"));
+    }
+    @Test
+    void negativeNumber(){
+        Exception e  = assertThrows(Exception.class, () -> {
+            calculator.retNumber("-5");
+        });
+        assertEquals("Negative number", e.getMessage());
+    }
+    @Test
+    void threeNumbers() throws Exception{
+        assertEquals(1204, calculator.retNumber("1000,200,4"));
+        assertEquals(120, calculator.retNumber("100\n20"));
+    }
+    @Test
+    void delimiterBeginning() throws Exception{
+        assertEquals(1400, calculator.retNumber("#1200,200"));
+    }
+    @Test
+    void nonThousands() throws Exception{
+        assertEquals(200, calculator.retNumber("1000,200"));
+    }
+}
