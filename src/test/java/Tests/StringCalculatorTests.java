3078f9eca848383fe3a45c37b5901065ac350973
Filip <filip.rak.20001@gmail.com>
2023-03-19 11:38:05 +0100

    fixing pom and tests

--- src/test/java/Tests/StringCalculatorTests.java
+++ src/test/java/Tests/StringCalculatorTests.java
@@ -11,36 +11,36 @@ public class StringCalculatorTests {
     StringCalculator calculator = new StringCalculator();
 
 
-    @Test
+    @org.junit.jupiter.api.Test
     void zeroElement() throws Exception{
             assertEquals(0, calculator.retNumber(""));
     }
-    @Test
+    @org.junit.jupiter.api.Test
     void oneNUmber()throws Exception{
             assertEquals(1, calculator.retNumber("1"));
     }
-    @Test
+    @org.junit.jupiter.api.Test
     void twoNumbers() throws Exception{
             assertEquals(7, calculator.retNumber("2,5"));
             assertEquals(15, calculator.retNumber("5\n10"));
     }
-    @Test
+    @org.junit.jupiter.api.Test
     void negativeNumber(){
         Exception e  = assertThrows(Exception.class, () -> {
             calculator.retNumber("-5");
         });
         assertEquals("Negative number", e.getMessage());
     }
-    @Test
+    @org.junit.jupiter.api.Test
     void threeNumbers() throws Exception{
         assertEquals(304, calculator.retNumber("100,200,4"));
         assertEquals(120, calculator.retNumber("100\n20"));
     }
-    @Test
+    @org.junit.jupiter.api.Test
     void delimiterBeginning() throws Exception{
         assertEquals(400, calculator.retNumber("#200,200"));
     }
-    @Test
+    @org.junit.jupiter.api.Test
     void nonThousands() throws Exception{
         assertEquals(200, calculator.retNumber("1000,200"));
     }
